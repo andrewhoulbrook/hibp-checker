4ff0669f8b6ebe72c76c1c0566d681a5d42d335c
Andrew Houlbrook <houlbrook.andrew>
2019-12-26 07:49:12 +0800

    first commit

--- test-scripts/adduser.sh
+++ test-scripts/adduser.sh
@@ -5,18 +5,18 @@
 # Check if user is root
 if [[ $(id -u) -eq 0 ]]
 then
-	# Get username and password
+    # Get username and password
     read -p "Enter username: " USERNAME
-	read -sp "Enter password: " PASSWORD    # don't print to terminal
+    read -sp "Enter password: " PASSWORD    # don't print to terminal
 
     # Check if username already exists, exit if username exists
     egrep "^${USERNAME}" /etc/passwd >/dev/null
-	if [[ $? -eq 0 ]]
+    if [[ $? -eq 0 ]]
     then
 		printf "\nERROR: username ${USERNAME} already exists.\n"
 		exit 1
-	else
-		# Check password against HIBP datasets
+    else
+        # Check password against HIBP datasets
         # Loop until user chooses password not in HIBP 
         BADPASSWORD=1
         while [[ "${BADPASSWORD}" == 1 ]]
@@ -44,8 +44,8 @@ then
         PASS=$(python -c "import os; import crypt; print crypt.crypt(\""${PASSWORD}"\", crypt.mksalt(crypt.METHOD_SHA512))")
 
         # Add user to the system with chosen username and password, check for errors
-		useradd -m -p "${PASS}" "${USERNAME}"
-		if [[ $? -eq 0 ]] 
+        useradd -m -p "${PASS}" "${USERNAME}"
+        if [[ $? -eq 0 ]] 
         then 
             printf "\nINFO: username ${USERNAME} has been added to system.\n" 
         else
@@ -53,6 +53,6 @@ then
         fi
     fi
 else
-	printf "\nERROR: root privilages are required to add a new user.\n"
-	exit 2
+    printf "\nERROR: root privilages are required to add a new user.\n"
+    exit 2
 fi
